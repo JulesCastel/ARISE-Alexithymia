b12348839eed5df3fe733e3c56776aa32e156c23
JulesCastel <juliancstllns@gmail.com>
2025-05-22 19:19:13 -0500

    Finishing out the project
    i should've been keeping up with updating the repo oops
    since the last push i have:
    - changed the code styling to my liking (using Allman brace style now)
    - restructured most of loop() into separate functions
    - created the playAudio() function to control playing an audio file and then ending the AudioStream so that the program can continue
    - handle the averages calculation with calculateAverages() and sampleData()
    - created another function for playing the [number].wav files in the right order
    
    tomorrow is the showcase!! yippee!!!!

--- main/ESP-A1S-firmware/ESP-A1S-firmware.ino
+++ main/ESP-A1S-firmware/ESP-A1S-firmware.ino
@@ -15,22 +15,23 @@
 
 int heart = 0;
 int force = 0;
-float heartAvg = 0.0f;
-float forceAvg = 0.0f;
 
 const char *startFilePath = "/";
-const char *ext = "mp3";
+const char *ext = "wav";
 AudioSourceSD source(startFilePath, ext, PIN_AUDIO_KIT_SD_CARD_CS);
 AudioBoardStream kit(AudioKitEs8388V1);
 WAVDecoder decoder;
 AudioPlayer player(source, kit, decoder);
 
-const unsigned long samplePeriod = 30000;
-const int sampleInterval = 100;
-const int maxSamples = samplePeriod / sampleInterval;
-int heartValues[maxSamples];
-int forceValues[maxSamples];
-int sampleCount = 0;
+unsigned int samplePeriod = 30000;
+unsigned const int sampleInterval = 100;
+unsigned int maxSamples;
+// these next two will be empty arrays, hence they are int*
+unsigned int* heartValues;
+unsigned int heartAverage;
+unsigned int* forceValues;
+unsigned int forceAverage;
+unsigned int sampleCount = 0;
 unsigned long startTime;
 
 // message data structure must match between TX'er and RX'er
@@ -48,7 +49,8 @@ void OnDataRX(const uint8_t *mac, const uint8_t *incomingData, int len)
   memcpy(&message, incomingData, sizeof(message));
   heart = message.heartData;
   force = message.forceData;
-  /*
+
+  /* these print statements were for debugging
   Serial.print("Bytes received: ");
   Serial.println(len);
 
@@ -62,100 +64,226 @@ void OnDataRX(const uint8_t *mac, const uint8_t *incomingData, int len)
   */
 }
 
-void calculateAvg()
+void sampleData()
 {
-  if (millis() - startTime < samplePeriod)
+  // get new sample?
+  if (sampleCount < maxSamples && millis() - startTime >= sampleCount * sampleInterval)
+  {
+    // sometimes the sensors will just read bad data. so i want to filter it out
+    // surely no one's heartrate should be below 50bpm or over 150bpm right?
+    if (heart > 50 && heart < 150 && force != 0)
     {
-      // get new sample?
-      if (sampleCount < maxSamples && millis() - startTime >= sampleCount * sampleInterval)
-        {
-          heartValues[sampleCount] = heart;
-          forceValues[sampleCount] = force;
-
-          Serial.print("Heart sample ");
-          Serial.print(sampleCount + 1);
-          Serial.print(": ");
-          Serial.println(heartValues[sampleCount]);
-
-          Serial.print("Force sample ");
-          Serial.print(sampleCount + 1);
-          Serial.print(": ");
-          Serial.println(forceValues[sampleCount]);
-
-          sampleCount++;
-        }
-    }
-
-  // done getting samples
-  else if (sampleCount > 0 && sampleCount >= maxSamples)
-    {
-      long heartSum = 0;
-      long forceSum = 0;
-
-      for (int i = 0; i < sampleCount; i++)
-        {
-          heartSum += heartValues[i];
-          forceSum += forceValues[i];
-        }
-      
-      int heartAverage = heartSum / sampleCount;
-      int forceAverage = forceSum / sampleCount;
-
-      Serial.println("Sampling complete!");
-      Serial.print("Average heart value: ");
-      Serial.println(heartAverage);
-      Serial.print("Average force value: ");
-      Serial.println(forceAverage);
-
+      heartValues[sampleCount] = heart;
+      forceValues[sampleCount] = force;
       sampleCount++;
     }
+
+    else
+    {
+      Serial.print("Bad sample data skipped! ->\t");
+      Serial.print("Heart data:\t");
+      Serial.print(heart);
+      Serial.print(" | Force data:\t");
+      Serial.println(force);
+      // increase the sampling window to ensure all needed samples are collected
+      samplePeriod += sampleInterval;
+    }
+  }
+
+  // done getting samples
+  if (sampleCount > 0 && sampleCount >= maxSamples)
+  {
+    unsigned long heartSum = 0;
+    unsigned long forceSum = 0;
+
+    for (int i = 0; i < sampleCount; i++)
+    {
+      heartSum += heartValues[i];
+      forceSum += forceValues[i];
+    }
+
+    heartAverage = heartSum / sampleCount;
+    forceAverage = forceSum / sampleCount;
+
+    Serial.println("Sampling complete!");
+    Serial.print("Average heart value: ");
+    Serial.println(heartAverage);
+    Serial.print("Average force value: ");
+    Serial.println(forceAverage);
+
+    sampleCount++;
+  }
+}
+
+// i couldn't find anything in the Audio-Tools library to play a file and then stop once it's done, so i made my own function for it
+void playAudio(const char* filePath)
+{
+
+  player.setPath(filePath);
+
+  Serial.print("Playing "); // file select
+  Serial.println(filePath);
+
+  player.play();
+  while (player.isActive())
+  {
+    player.copy();
+
+    // if the audio track has stopped, deactivate the AudioPlayer, letting the program to continue
+    if (!player.copy())
+    {
+      player.stop();
+    }
+  }
+
+  Serial.print(filePath);
+  Serial.println(" has finished playing.");
+}
+
+void calculateAverages()
+{
+  startTime = millis();
+  Serial.print("Sampling start time: ");
+  Serial.println(startTime);
+
+  Serial.println("Sampling...");
+  while (sampleCount <= maxSamples)
+  {
+    sampleData();
+
+    if (millis() - startTime >= 30000) // if 30 seconds have passed
+    {
+      Serial.println("Sampling took too long, skipping stage.");
+      break;
+    }
+  }
+}
+
+void playAverageAudio(unsigned int avg)
+{
+  playAudio("/average.wav");
+
+  if (avg < 50)
+  {
+    playAudio("/unable-calculate.wav");
+    return;
+  }
+
+  if (avg >= 100)
+  {
+    playAudio("/over-100.wav");
+    return;
+  }
+  else if (avg >= 90)
+  {
+    playAudio("/90.wav");
+  }
+  else if (avg >= 80)
+  {
+    playAudio("/80.wav");
+  }
+  else if (avg >= 70)
+  {
+    playAudio("/70.wav");
+  }
+  else if (avg >= 60)
+  {
+    playAudio("/60.wav");
+  }
+  else if (avg >= 50)
+  {
+    playAudio("/50.wav");
+  }
+
+  if (avg % 10 == 9)
+  {
+    playAudio("/9.wav");
+  }
+  else if (avg % 10 == 8)
+  {
+    playAudio("/8.wav");
+  }
+  else if (avg % 10 == 7)
+  {
+    playAudio("/7.wav");
+  }
+  else if (avg % 10 == 6)
+  {
+    playAudio("/6.wav");
+  }
+  else if (avg % 10 == 5)
+  {
+    playAudio("/5.wav");
+  }
+  else if (avg % 10 == 4)
+  {
+    playAudio("/4.wav");
+  }
+  else if (avg % 10 == 3)
+  {
+    playAudio("/3.wav");
+  }
+  else if (avg % 10 == 2)
+  {
+    playAudio("/2.wav");
+  }
+  else if (avg % 10 == 1)
+  {
+    playAudio("/1.wav");
+  }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Info);
+  AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Warning);
 
   auto cfg = kit.defaultConfig(TX_MODE);
   cfg.sd_active = true;
   kit.begin(cfg);
 
-  player.setVolume(0.5);
+  // TODO find a way to change the volume without having to recompile bc it's so annoying LOL
+  player.setVolume(0.2);
   player.begin();
-
-  // file select
-  player.setPath("/activated.wav");
-
 
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != ESP_OK)
-    {
-      Serial.println("Error initializing ESP-NOW");
-      return;
-    }
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
 
   // message receive callback function = onDataRX()
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRX));
 
-  startTime = millis();
+  maxSamples = samplePeriod / sampleInterval;
+  heartValues = new unsigned int[maxSamples];
+  forceValues = new unsigned int[maxSamples];
 }
 
 void loop()
 {
-  while (sampleCount <= maxSamples)
-    {
-      calculateAvg();
-    }
-  
-  while (player.isActive())
-    {
-      player.copy();
-    }
-  
+  playAudio("/startup.wav");
+
+  calculateAverages();
+
+  playAverageAudio(heartAverage);
+
+  // TODO the actual algorithm for calculating mood. for now, we'll assume "stressed" as the mood
+  // everyone's kinda stressed anyway, right?
+
+  playAudio("/stressed.wav");
+
+  playAudio("/goodbye.wav");
+
+  // do nothing once everything is over
+  Serial.println("Process complete. Standing by....");
+  while (true) {}
+
   /*
     How I want to structure this:
-    2) wait 30 seconds to gather average heart rate and force on the sensor
-    3) then play the mood that it "detects" with the force sensor parameter affecting the audio
+    2) wait 30 seconds to gather average heart rate and force on the sensor - DONE
+    3) then play the mood that it "detects" with the force sensor parameter affecting the audio - DONE kinda
   */
 }
